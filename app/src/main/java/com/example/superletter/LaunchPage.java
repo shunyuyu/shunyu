36ccc0e171d21363b76c30ec28a54044da58574f
xushunyu <2631582891@qq.com>
2020-06-09 22:01:45 +0800

    启动页面变成launchPage页面：设置图片为启动页面 两秒后跳转到LoginActivity页面

---
+++ app/src/main/java/com/example/superletter/LaunchPage.java
@@ -0,0 +1,29 @@
+package com.example.superletter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+/**
+ * @author xushunyu
+ */
+public class LaunchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_page);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(LaunchPage.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
--- app/src/main/java/com/example/superletter/MainActivity.java
+++ app/src/main/java/com/example/superletter/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
