e1de388190f2c0bebcc9edecf43b31401ba32048
xushunyu <2631582891@qq.com>
2020-06-10 14:24:44 +0800

    RegisterActivity注册页面布局

---
+++ app/src/main/java/com/example/superletter/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.superletter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author xushunyu
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
