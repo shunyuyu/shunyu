7cd7bbe5216f1baef848d5c3481ba203d4a51aad
xushunyu <2631582891@qq.com>
2020-06-10 13:23:16 +0800

    LoginActivity页面:实现密码验证码登录的切换

--- app/src/main/java/com/example/superletter/LoginActivity.java
+++ app/src/main/java/com/example/superletter/LoginActivity.java
@@ -3,17 +3,42 @@ package com.example.superletter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import com.example.superletter.databinding.ActivityLoginBinding;
 
 /**
  * @author xushunyu
  */
 public class LoginActivity extends AppCompatActivity {
-
+    private ActivityLoginBinding mBinding;
+    private boolean bad = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.textSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bad) {
+                    mBinding.textSms.setText("用密码登录");
+                    mBinding.textPwd.setText("验证码");
+                    mBinding.editPwd.setHint("请输入验证码");
+                    bad=false;}
+                else{
+                    mBinding.textSms.setText("用验证码登录");
+                    mBinding.textPwd.setText("密码");
+                    mBinding.editPwd.setHint("请输入密码");
+                    bad=true;
+                }
+            }
+        });
+
+
+
 
     }
 }
