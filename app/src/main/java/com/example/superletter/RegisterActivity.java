32c4ab950801bf0c34c16df244e05299514cba16
xushunyu <2631582891@qq.com>
2020-06-11 11:28:41 +0800

    AgreementActivity页面布局和实现注册页面点击协议跳转到协议页面

--- app/src/main/java/com/example/superletter/RegisterActivity.java
+++ app/src/main/java/com/example/superletter/RegisterActivity.java
@@ -2,16 +2,30 @@ package com.example.superletter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.superletter.databinding.ActivityLoginBinding;
+import com.example.superletter.databinding.ActivityRegisterBinding;
 
 /**
  * @author xushunyu
  */
 public class RegisterActivity extends AppCompatActivity {
-
+    private ActivityRegisterBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        mBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.textAgreement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(RegisterActivity.this,AgreementActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
